37f58f44925de750a03585929b3d0abc674dffca
Dervin Tian <dervint@umich.edu>
2026-06-19 12:09:13 -0400

    Updated documentation for COPY and MOVE keywords

---
+++ docs/MOVE/README.md
@@ -0,0 +1,12 @@
+# MOVE # 
+
+## Purpose ##
+The purpose of the MOVE keyword is to move the data from an original table over to a new table, leaving the original empty.
+
+## Usage ##
+```cpp
+MOVE (source_table, dest_table);
+...
+```
+- **source_name:** represents the name of the table that we want to source the data from.
+- **dest_name:** represents the name of the table where we want to move the source data to.
---
+++ docs/MOVE/move.cpp
@@ -0,0 +1,80 @@
+/*
+Implementation for the move function
+Arguments:
+    - arguments: contains the table that we want to move, and the table where want it to be moved to
+*/
+void move_qmt(const cmd_args &arguments){
+    std::cout << "Running move implementation, can fill out semantics later\n";
+    executing_line_num++; // update the execution line number
+
+    // Do error checking to ensure that the database and the tables are valid paths
+    if(!valid_pathname(db_path)){
+        exit_with_error(INVALID_DATABASE_NAME, "");
+    }
+
+    if(!valid_table(arguments.move.source_table)){
+        exit_with_error(INVALID_TABLENAME, arguments.move.source_table);
+    }
+
+    if(!valid_table(arguments.move.dest_table)){
+        exit_with_error(INVALID_TABLENAME, arguments.move.dest_table);
+    }
+
+    std::string source_table_path = db_path + "/" + arguments.move.source_table;
+    std::string source_schema_path = db_path + "/schemas/" + arguments.move.source_table;
+
+    std::string dest_table_path = db_path + "/" + arguments.move.dest_table;
+    std::string dest_schema_path = db_path + "/schemas/" + arguments.move.dest_table;
+
+    if(!fs::exists(source_table_path)){
+        exit_with_error(NULL_TABLE, arguments.move.source_table);
+    }
+
+    if(!fs::exists(dest_table_path)){
+        exit_with_error(NULL_TABLE, arguments.move.dest_table);
+    }
+
+    // read in schema, so that we can compare and make sure that we can actually copy the tables over
+    std::vector<std::string> source_tbl_schema_types = read_schema(source_schema_path)[1];
+    std::vector<std::string> dest_tbl_schema_types = read_schema(dest_schema_path)[1];
+
+    if(source_tbl_schema_types.size() != dest_tbl_schema_types.size()){
+        exit_with_error(DIFF_SCHEMAS, "");
+    }
+
+    // Go through the types and if there is a type conversion that cannot be made, then error out
+    // Also copy values completely later, so that we can avoid having partially copied over tables and stuff
+    for(size_t i = 0; i < source_tbl_schema_types.size(); ++i){
+        if(dest_tbl_schema_types[i] == "string"){
+            continue; // Anything can convert into a string
+        }
+        else if(dest_tbl_schema_types[i] == "char"){
+            if(source_tbl_schema_types[i] != "char"){
+                exit_with_error(DIFF_SCHEMAS, "");
+            }
+        }
+        else if(dest_tbl_schema_types[i] == "bool"){
+            if(source_tbl_schema_types[i] != "bool"){
+                exit_with_error(DIFF_SCHEMAS, "");
+            }
+        }
+        else if(dest_tbl_schema_types[i] == "int" || dest_tbl_schema_types[i] == "double"){
+            if(source_tbl_schema_types[i] != "int" && source_tbl_schema_types[i] != "double"){
+                exit_with_error(DIFF_SCHEMAS, "");
+            }
+        }
+        else{
+            exit_with_error(UNKNOWN_TYPE, dest_tbl_schema_types[i]);
+        }
+    }
+
+    // empty constraints for now, but actually could be a good idea to have some constraints, like only copy select columns over
+    std::vector<std::vector<std::string>> original_table = from_qmt(source_table_path, std::vector<select_additional_args>{});
+    std::vector<std::vector<std::string>> empty_table;
+
+    // Going to need to make these atomic somehow
+    write_table_to_disk(original_table, arguments.move.dest_table);
+    write_table_to_disk(empty_table, arguments.move.source_table);
+
+    return;
+}
