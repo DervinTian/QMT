7dc7b3d262054fd8aada5b2a5ce88627abd04a7c
Dervin Tian <dervint@umich.edu>
2026-06-30 21:23:43 -0400

    Added get_free_blocks() for initialziation of virtual disk

--- codebase/globals_disk.cpp
+++ codebase/globals_disk.cpp
@@ -3,84 +3,101 @@
 
 #include "globals_disk.h"
 
-const uint64_t DISK_SIZE = 1048576; // Just set it to 1MB for now
-const uint32_t INODE_MAX_BLOCKS = DISK_SIZE / BLOCK_SIZE;
-const uint32_t BLOCK_SIZE = 512;
-const uint32_t MAX_OWNER_SIZE = 11;
-const uint32_t MAX_TABLENAME_SIZE = 47;
-
-/*
-Inode layout size:
-4 bytes for size
-11 + 1 bytes for owner name size
-47 + 1 bytes for table name size
-448 bytes left for availble disk blocks (MAX 112 blocks per inode)
-*/
-
 std::unordered_set<int> free_disk_blocks;
 
 const std::string VM_DISK = "vm_disk.bin";
 
 void convert_block_to_inode(inode &curr_inode, const char* block){
     int running_idx = 0;
-
-    uint32_t size = 0;
     std::string attr;
-    for(int i = running_idx; i < sizeof(uint32_t) + running_idx; ++i){
-        attr += block[i];
-        running_idx++;
-    }
 
-    size = std::stoi(attr, nullptr, 16);
-    curr_inode.size = size;
+    for(int i = running_idx; i < sizeof(char) + running_idx; ++i){
+        attr += block[i];
+    }
+    running_idx = sizeof(char) + running_idx;
+    curr_inode.type = attr[0];
+
+    if(curr_inode.type == '\0'){
+        // This means that we do not have an inode and that we should not be looking at this data so return and make sure we don't reference it
+        return;
+    }
     attr.clear();
+    
+    // AI helped me read in the bytes into a big endian format, basically since the left are the big bits, we need to bit shift to the right over, and then or everything together to concatenate them into a single size value
+    uint32_t size =
+        (static_cast<uint8_t>(block[1]) << 24) |
+        (static_cast<uint8_t>(block[2]) << 16) |
+        (static_cast<uint8_t>(block[3]) << 8)  |
+        static_cast<uint8_t>(block[4]);
 
-    for(int i = running_idx; i < MAX_OWNER_SIZE + running_idx; ++i){
+    running_idx = sizeof(uint32_t) + running_idx;
+    curr_inode.size = size;
+
+    for(int i = running_idx; i < MAX_OWNER_SIZE + 1 + running_idx; ++i){
         attr += block[i];
-        running_idx++;
     }
+    running_idx = MAX_OWNER_SIZE + 1 + running_idx;
     for(int i = 0; i < attr.size(); ++i){
         curr_inode.owner[i] = attr[i];
     }
     attr.clear();
 
-    for(int i = running_idx; i < MAX_OWNER_SIZE + running_idx; ++i){
+    for(int i = running_idx; i < MAX_TABLENAME_SIZE + 1 + running_idx; ++i){
         attr += block[i];
-        running_idx++;
     }
+    running_idx = MAX_TABLENAME_SIZE + 1 + running_idx;
     for(int i = 0; i < attr.size(); ++i){
         curr_inode.tbl_name[i] = attr[i];
     }
     attr.clear();
 
-    int inode_blocks_idx = 0;
-    for(uint32_t i = running_idx; i < size + running_idx; ++i){
-        attr += block[i];
-
-        if(attr.size() == sizeof(uint32_t)){
-            // We have hit another block num, so read it in
-            uint32_t block_num = std::stoi(attr, nullptr, 16);
-            attr.clear();
-            curr_inode.free_blocks[inode_blocks_idx] = block_num;
-            inode_blocks_idx++;
-        }
+    uint32_t curr_inode_blocks_idx = 0;
+    for(uint32_t i = running_idx; i < (size * sizeof(uint32_t)) + running_idx;){
+        uint32_t inode_block_num =
+            (static_cast<uint8_t>(block[i + 0]) << 24) |
+            (static_cast<uint8_t>(block[i + 1]) << 16) |
+            (static_cast<uint8_t>(block[i + 2]) << 8)  |
+            static_cast<uint8_t>(block[i + 3]);
+        i += 4;
+        curr_inode.blocks[curr_inode_blocks_idx] = inode_block_num;
+        curr_inode_blocks_idx++;
     }
     
     return;
 }
 
-std::set<int> get_free_blocks(){
+std::unordered_set<int> get_free_blocks(){
     char block[BLOCK_SIZE];
 
-    std::set<int> curr_free_blocks;
+    std::unordered_set<int> all_free_blocks;
+    std::unordered_set<int> curr_used_blocks;
+    std::unordered_set<int> free_blocks;
     std::ifstream file(VM_DISK, std::ios::binary);
 
-    for(int i = 0; i < INODE_MAX_BLOCKS; ++i){
-        free_disk_blocks.insert(i);
+    for(int i = 0; i < NUM_DISK_BLOCKS; ++i){
+        all_free_blocks.insert(i);
     }
 
     int curr_block_num = 0;
-    file.read(block, BLOCK_SIZE);
+    while(file.read(block, BLOCK_SIZE)){
+        inode curr_inode;
+        convert_block_to_inode(curr_inode, block);
+        if(curr_inode.type != '\0'){
+            curr_used_blocks.insert(curr_block_num);
+            for(int i = 0; i < curr_inode.size; ++i){
+                curr_used_blocks.insert(curr_inode.blocks[i]);
+            }
+        }
 
-    return curr_free_blocks;
+        curr_block_num++;
+    }
+
+    for(auto& x : all_free_blocks){
+        if(curr_used_blocks.find(x) == curr_used_blocks.end()){
+            // means that we didn't find the block number in the curr_used_blocks, it is free
+            free_blocks.insert(x);
+        }
+    }
+
+    return free_blocks;
 }
--- codebase/globals_disk.h
+++ codebase/globals_disk.h
@@ -10,19 +10,31 @@
 #include <unordered_set>
 #include <set>
 
-// Have a root inode that acts as like the "root" directory, that holds all of the tables
+const uint64_t DISK_SIZE = 1048576; // Just set it to 1MB for now
+const uint32_t BLOCK_SIZE = 512;
+const uint32_t MAX_OWNER_SIZE = 10;
+const uint32_t MAX_TABLENAME_SIZE = 47;
+const uint32_t INODE_MAX_BLOCKS = (DISK_SIZE - 1 - 4 - (MAX_OWNER_SIZE + 1) - (MAX_TABLENAME_SIZE + 1)) / BLOCK_SIZE;
+const uint32_t NUM_DISK_BLOCKS = DISK_SIZE / BLOCK_SIZE;
+
+/*
+Inode layout size:
+1 byte for the type
+4 bytes for size
+10 + 1 bytes for owner name size
+47 + 1 bytes for table name size
+448 bytes left for availble disk blocks (MAX 112 blocks per inode)
+*/
+
+// If a disk block is empty, then the first character of that disk block read in will be '\0', for both inodes and regular table disk blocks
 
 struct inode {
+    char type;
     uint32_t size;
     char owner[MAX_OWNER_SIZE];
     char tbl_name[MAX_TABLENAME_SIZE];
-    uint32_t free_blocks[INODE_MAX_BLOCKS];
+    uint32_t blocks[INODE_MAX_BLOCKS];
 };
-
-extern const uint32_t BLOCK_SIZE;
-extern const uint64_t DISK_SIZE; // Just set it to 1MB for now
-extern const uint32_t MAX_OWNER_SIZE;
-extern const uint32_t MAX_TABLENAME_SIZE;
 
 extern const uint32_t INODE_MAX_BLOCKS;
 
@@ -31,4 +43,4 @@ extern std::unordered_set<int> free_disk_blocks;
 extern const std::string VM_DISK;
 
 void convert_block_to_inode(inode &curr_inode);
-std::set<int> get_free_blocks();
+std::unordered_set<int> get_free_blocks();
